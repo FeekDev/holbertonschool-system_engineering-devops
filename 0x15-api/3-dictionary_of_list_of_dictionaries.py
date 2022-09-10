207620d07faa8bf31134b98be33a90f5204eb8c9
felipeholbi <4758@holbertonstudents.com>
2022-09-10 10:04:48 -0500

    point 3
    dictionary of list dictionary all employees

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script display the tasks completed
+"""
+from requests import get
+from json import dump
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = get(url)
+    users = response.json()
+
+    data = {}
+    for usr in users:
+        usr_id = usr.get('id')
+        username = usr.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(usr_id)
+        url = url + '/todos'
+        response = get(url)
+        tasks = response.json()
+        data[usr_id] = []
+        for task in tasks:
+            data[usr_id].append({
+                            "task": task.get('title'),
+                            "completed": task.get('completed'),
+                            "username": username
+                            })
+    with open('todo_all_employees.json.json'.format(usr), 'w') as f:
+        dump(data, f)
