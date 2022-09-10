1da42bb5ebb0c2413728932a27a2e611c4ba028d
felipeholbi <4758@holbertonstudents.com>
2022-09-09 21:33:02 -0500

    fix point 1
    change the name for username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,13 +3,12 @@
 This script display the tasks completed
 """
 from requests import get
-import csv
 from sys import argv
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
     response = get(url)
-    name = response.json().get('name')
+    username = response.json().get('username')
 
     url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])
     response = get(url)
@@ -18,5 +17,5 @@ if __name__ == '__main__':
     with open('{}.csv'.format(argv[1]), mode='w') as f:
         for task in tasks:
             f.write('"{}","{}","{}","{}"\n'
-                    .format(argv[1], name, task.get('completed'),
+                    .format(argv[1], username, task.get('completed'),
                             task.get('title')))
